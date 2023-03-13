ba49853879a844bba3c8d8554a2abf7a126516b2
Felipe Bueno <felipebueno2201@gmail.com>
2023-03-13 03:58:05 -0300

    fixed states

--- src/pages/Dashboard/Hotel/Booking.js
+++ src/pages/Dashboard/Hotel/Booking.js
@@ -12,32 +12,33 @@ export default function Booking() {
   const token = useToken();
   const config = { headers: { Authorization: `Bearer ${token}` } };
   const { booking } = useLocation().state;
-  const [ infos, setInfos] = useState([]);
-  const [ Room ] = useState(booking.Room);
+  const [infos, setInfos] = useState([]);
+  const [Room, setRoom] = useState(booking.Room);
   const { change, setChange } = useContext(ChangeContext);
   const capacity = { 1: 'Single', 2: 'Double', 3: 'Triple' };
-    
+
   useEffect(async() => {
     try {
       const res = await instance.get(`/hotels/${Room.hotelId}`, config);
       setInfos(res.data);
+      setRoom(booking.Room);
     } catch (err) {
       console.log(err);
     }
   }, [Room, change, booking]);
-  
+
   const { name, image, Rooms = [] } = infos;
 
   function reservationsRooms() {
-    const room = Rooms.find( r => r.name === Room.name);
-    if(room) {
+    const room = Rooms.find((r) => r.name === Room.name);
+    if (room) {
       const reservations = room.Booking.length;
-      if(reservations === 1) return 'Somente você';
+      if (reservations === 1) return 'Somente você';
       return `Você e mais ${reservations - 1}`;
     }
 
     return '';
-  };
+  }
 
   function changeRoom() {
     setChange(false);
@@ -45,7 +46,7 @@ export default function Booking() {
   }
 
   function capacityRoom() {
-    return Room?.capacity && capacity[Room.capacity] || '-';
+    return (Room?.capacity && capacity[Room.capacity]) || '-';
   }
 
   return (
@@ -56,12 +57,15 @@ export default function Booking() {
       </ContainerTicket>
       <Hotelbutton>
         <HotelImage>
-          <img src={ image }></img>
-          <h2>{ name }</h2>
+          <img src={image}></img>
+          <h2>{name}</h2>
         </HotelImage>
         <HotelData>
           <h5>Quarto reservado:</h5>
-          <p> { Room.name } ({ capacityRoom() })</p>
+          <p>
+            {' '}
+            {Room.name} ({capacityRoom()})
+          </p>
         </HotelData>
         <HotelData>
           <h5>Pessoas no seu quarto:</h5>
@@ -71,56 +75,56 @@ export default function Booking() {
       <Reserve onClick={changeRoom}>TROCAR DE QUARTO</Reserve>
     </>
   );
-};
+}
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 20px !important;
 `;
 const Hotelbutton = styled.button`
-    background-color: #FFEED2;
-    width: 196px;
-    height: 264px;
-    margin-right: 1rem;
-    margin-top: 1rem;
-    margin-bottom: 2rem;
-    border: 1px solid #cecece;
-    border-radius: 20px;
-    font-size: 16px;
-    display: flex;
-    gap: 5px;
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-   
-    p {
-        color: #3c3c3c;
-        font-size: 12px;
-        font-weight: 400;
-    }
-    img {
-        width: 168px;
-        height: 109px;
-        border-radius: 5px;
-    }
-    h5 {
-        font-weight: 700;
-        font-size: 12px;
-    }
-    h2 {
-        color: #343434;
-        font-weight: 400;
-        font-size: 20px;
-    }
+  background-color: #ffeed2;
+  width: 196px;
+  height: 264px;
+  margin-right: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+  border: 1px solid #cecece;
+  border-radius: 20px;
+  font-size: 16px;
+  display: flex;
+  gap: 5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  p {
+    color: #3c3c3c;
+    font-size: 12px;
+    font-weight: 400;
+  }
+  img {
+    width: 168px;
+    height: 109px;
+    border-radius: 5px;
+  }
+  h5 {
+    font-weight: 700;
+    font-size: 12px;
+  }
+  h2 {
+    color: #343434;
+    font-weight: 400;
+    font-size: 20px;
+  }
 `;
 const HotelData = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-start;
-    flex-direction: column;
-    width: 170px;
-    margin-top: 10px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 170px;
+  margin-top: 10px;
 `;
 const HotelImage = styled.div`
   display: flex;
